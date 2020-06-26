3d714842f2f05bcdebe0a9b57c8a719832774ee8
Yangfan Chang <yangfanchang@192-168-1-103.tpgi.com.au>
2020-06-26 23:17:40 +1000

    Rendered about page.

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("about");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
