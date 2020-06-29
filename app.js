b1133f558a0a6c320d8693190a92093577113270
YangfanZhang <yf_chang@icloud.com>
2020-06-29 18:48:36 +1000

    Added 2 more pages

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res){
     res.render("about");
 });
 
+app.get("/projects", function(req, res){
+    res.render("projects");
+});
+
+app.get("/passion", function(req, res){
+    res.render("passion");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 })
