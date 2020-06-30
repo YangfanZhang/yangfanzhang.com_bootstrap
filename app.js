209546b7105b6845e5c5f13fa7371d105a5f1116
YangfanZhang <yf_chang@icloud.com>
2020-06-30 14:58:22 +1000

    Added port and Procfile for heroku

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get("/passion", function(req, res){
     res.render("passion");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000");
 })
